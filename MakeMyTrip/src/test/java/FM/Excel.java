be14e89d0272f4f1a4537b81ffdbc7ada063aac8
AW104_PC1 <AW104_PC1@Infva05834>
2018-10-05 14:17:28 +0530

    VV

--- MakeMyTrip/src/test/java/FM/Excel.java
+++ MakeMyTrip/src/test/java/FM/Excel.java
@@ -2,15 +2,16 @@ package FM;
 
 import java.io.FileInputStream;
 
-
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Excel
 {
 	
 	static Sheet s;
+	static Workbook wb;
 	public static void ExcelConfig(String Location ,String SheetName)
 	{
 		try
@@ -18,13 +19,13 @@ public class Excel
 		FileInputStream I=new FileInputStream(Location);
 		if(Location.endsWith("xls")==true)
 		{
-			HSSFWorkbook wb=new HSSFWorkbook(I);
+			 wb=new HSSFWorkbook(I);
 		
 			s=wb.getSheet(SheetName);
 		}
 		else if(Location.endsWith("xlsx")==true)
 		{
-			XSSFWorkbook wb=new XSSFWorkbook(I);
+			 wb=new XSSFWorkbook(I);
 			
 			s=wb.getSheet(SheetName);
 		}
